18699a234697b72827777f5a321cc7423d239105
stephan-93 <eammemp@gmail.com>
2017-11-28 16:39:17 -1000

    small progress on home pagin a maxin of side art

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
         menubutton[i].onclick = function(){
             this.classList.toggle("expanded");
         }
-    } 
-    
+    }
+
     //TOGGLE ACCORDIAN
     var acc = document.querySelectorAll('.accordian');
     var i;
@@ -17,5 +17,23 @@ document.addEventListener('DOMContentLoaded', function() {
             this.parentElement.classList.toggle("open");
         }
     }
-    
+
+
 })
+
+document.onscroll = function() {
+
+  //console.log("scrolling!");
+
+  var anim = document.querySelectorAll('.side-art');
+  var i;
+  for (i = 0; i < anim.length; i++) {
+        anim[i].classList.addClass('off');
+  }
+};
+
+$('p').each(function(){
+    var string = $(this).html();
+    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+    $(this).html(string);
+});
