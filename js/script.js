3f1fa9246583cba4c70d2f32edb3d44338fbaacc
stephan-93 <eammemp@gmail.com>
2017-12-04 12:34:45 -1000

    added most of home pages, started adding js, padding mobile querries for side art

--- js/script.js
+++ js/script.js
@@ -1,39 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    //TOGGLE MENU NAV
-    var menubutton = document.querySelectorAll('.menu-button');
-    var i;
-    for (i = 0; i < menubutton.length; i++) {
-        menubutton[i].onclick = function(){
-            this.classList.toggle("expanded");
-        }
+  //TOGGLE MENU NAV
+  var menubutton = document.querySelectorAll('.menu-button');
+  var i;
+  for (i = 0; i < menubutton.length; i++) {
+    menubutton[i].onclick = function(){
+      this.classList.toggle("expanded");
     }
+  }
 
-    //TOGGLE ACCORDIAN
-    var acc = document.querySelectorAll('.accordian');
-    var i;
-    for (i = 0; i < acc.length; i++) {
-        acc[i].getElementsByTagName('h4')[0].onclick = function(){
-            this.parentElement.classList.toggle("open");
-        }
+  //TOGGLE ACCORDIAN
+  var acc = document.querySelectorAll('.accordian');
+  var i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].getElementsByTagName('h4')[0].onclick = function(){
+      this.parentElement.classList.toggle("open");
     }
+  }
 
 
 })
 
-document.onscroll = function() {
+// document.onscroll = function() {
+//
+//   //console.log("scrolling!");
+//
+//   var anim = document.querySelectorAll('.side-art');
+//   var i;
+//   for (i = 0; i < anim.length; i++) {
+//         anim[i].classList.addClass('off');
+//   }
+// };
 
-  //console.log("scrolling!");
-
-  var anim = document.querySelectorAll('.side-art');
-  var i;
-  for (i = 0; i < anim.length; i++) {
-        anim[i].classList.addClass('off');
-  }
-};
-
+//REPLACE LAST SPACE OF PARAGUAPHS WITH NON-BREAK SPACE
 $('p').each(function(){
-    var string = $(this).html();
-    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
-    $(this).html(string);
+  var string = $(this).html();
+  string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+  $(this).html(string);
 });
+
+//RESIZE OVERSIDED TEXT IN LISTING
+
+var square = $('#all-events .event-listing li');
+
+$(document).ready(function(){
+  alert("Hello World!");
+  // square.each(function(){
+  //     if (square.width() != square.height()) {
+  //       $('#all-events .event-listing li .h1').addClass("oversized");
+  //     } else {
+  //       $('#all-events .event-listing li .h1').removeClass("oversized");}
+  // })
+})
